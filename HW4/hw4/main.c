364c46e7c4a2bc128bf41e36e0c6f64be153a652
ben <limhanyangb@gmail.com>
2018-04-17 12:20:57 -0500

    Update working code

--- HW4/hw4/main.c
+++ HW4/hw4/main.c
@@ -2,8 +2,6 @@
 #include<sys/attribs.h>  // __ISR macro
 #include <math.h>
 
-#define PI 3.14159265
-
 // DEVCFG0
 #pragma config DEBUG = OFF // no debugging
 #pragma config JTAGEN = OFF // no jtag
@@ -59,7 +57,7 @@ void spiConfig(void){
     SPI1BRG=1; // What is the peripheral bus clock?
     SPI1STATbits.SPIROV = 0;
     SPI1CONbits.MODE32=0;
-    SPI1CONbits.MODE16=1;
+    SPI1CONbits.MODE16=0; // 8bit mode
     SPI1CONbits.MSTEN=1;
     // Fsck = Fpb/(2*(SPI1BRG+1))
     // Fsck = 48Mhz/(2*(1+1)) = 12Mhz
@@ -67,30 +65,31 @@ void spiConfig(void){
     SPI1CONbits.ON=1; // Turn on SPI
 }
 
-void spiSend(char write){
-    LATBbits.LATB15=0; // down
+char spiSend(char write){
+    
     SPI1BUF = write; // just send and forget
-    //while(!SPI1STATbits.SPIRBF){;} // wait until we receive data?
+    while(!SPI1STATbits.SPIRBF){;} // wait until we receive data?
+    return SPI1BUF;
     //SPI1BUF; // Ignore garbage
     //SPI1BUF = 5; //Write garbage
     //LATBbits.LATB15=1; // up
     //return SPI1BUF;
 }
 
-void setVoltage(char channel, char voltage){
-    SPI1BUF; // read to clear data
+void setVoltage(char channel, int voltage){
+    /*
+     * t = a <<15
+     * t = t | 0b0111000000000000
+     * t = t | ((v&0b1111111111)<<2)
+     */
     LATBbits.LATB15=0; // Low to enable data
-    uint16_t carrier;
+    int bitvalue = channel<<15;
+    bitvalue = bitvalue | 0b0111000000000000;
+    bitvalue = bitvalue | ((voltage&0b1111111111)<<2);
     
-    if (channel == 'A'){
-        carrier=0b0<<15;
-    }
-    else if (channel == 'B'){
-        carrier=0b1<<15;
-    }
-    carrier = carrier | 0b111<<12 | voltage <<4; // Unbuffered
-    SPI1BUF = carrier;
-    while(!SPI1STATbits.SPIRBF);
+   
+    spiSend(bitvalue>>8);
+    spiSend(bitvalue);
     LATBbits.LATB15=1; // up!
 }
 
@@ -118,12 +117,12 @@ int main() {
     //TRISB = 0b0<<14; // RB14 - SCK
     //TRISB = 0b0<<8; // RB8 MOSI
     //TRISB = 0b0<<15; // RB15 - CS
-    LATAbits.LATA4 = 1; //HIGH
+    LATAbits.LATA4 = 0; //HIGH
     LATBbits.LATB15 = 0; // LOW
     
     TRISB = 0b1<<4; // INIT B4 to input
-    ANSELA = 0;
-    ANSELB = 0;
+    //ANSELA = 0;
+    //ANSELB = 0;
     SS1Rbits.SS1R = 0b0011;
     SDI1Rbits.SDI1R = 0b0100;
     RPA1Rbits.RPA1R = 0b0011;
@@ -138,54 +137,79 @@ int main() {
     //
     //SPI1_IO('A');
     //setVoltage('A',128);
+    SPI1CON=0;
     SPI1BUF;
-    SPI1BRG=0x4;
+    SPI1BRG=0x1;
     SPI1STATbits.SPIROV = 0;
     SPI1CONbits.MODE32=0;
-    SPI1CONbits.MODE16=1;
+    SPI1CONbits.MODE16=0;
+    SPI1CONbits.CKE = 1;
     SPI1CONbits.MSTEN=1;
     SPI1CONbits.MSSEN=0; // Pin controlled by port function
     SPI1CONbits.ON = 1;
     
     
-    setVoltage('B',0);
-    setVoltage('A',0);
+    //setVoltage('B',0);
+    //setVoltage('A',1022);
     
     /*
     LATBbits.LATB15 = 0; // LOW
-    SPI1BUF = 0| 0b11111111<<4 | 0b1111<<12;
+    SPI1BUF = 0| 0b1111<<13 | 512<<3 ;
     while(!SPI1STATbits.SPIRBF);
     LATBbits.LATB15 = 1; // HIGH
     
     SPI1BUF;
     
     LATBbits.LATB15 = 0; // LOW
-    SPI1BUF = 0| 0b11111111<<4 | 0b0111<<12;
+    SPI1BUF = 0| 0b0111<<13 | 1023<<3 ; // A (by datasheet) -  but channel 2?? so probably B?
     while(!SPI1STATbits.SPIRBF);
     LATBbits.LATB15 = 1; // HIGH
-    */
+    
+    SPI1BUF;
+    
     int index = 0;
     int increment = 1;
-
+    */
+    setVoltage(1,255);
+    setVoltage(0,512);
+    
+    int i = 0;
+    
+    int coeff = 0;
     while(1) {
 	// use _CP0_SET_COUNT(0) and _CP0_GET_COUNT() to test the PIC timing
 	// remember the core timer runs at half the sysclk
+        _CP0_SET_COUNT(0); // reset count
         
-        if(_CP0_GET_COUNT()> (48000000/2)/(2*10)){ // 1kHz
-            if (increment > 10){
-                increment = 0;
-            }
-            index = (int)255*sin(2*PI*(increment/10));
-            setVoltage('A',0);
-            increment++;
+        
+        float f = 512.0 + 511.0*sin(i*2.0*3.14/4000.0);
+        setVoltage(0,(int)f);
+        float tri =  coeff/4000.0*1023.0;
+        
+        i++;
+        coeff = i;
+        if(i>8000){
+            i = 0;
+            coeff = i;
+        }
+        else if (i>3999){
+            coeff = 8000-i;
+        }
+        setVoltage(1,(int)tri);
+        LATAINV=0b1<<4; //toggle pin 4
+        
+        if(_CP0_GET_COUNT()> (48000000/2)/(1000)){ // 1kHz
+            
+           
+            
             
             
             
 
             
             
-            //LATAINV=0b1<<4; //toggle pin 4
-            _CP0_SET_COUNT(0); // reset count
+            
+            
         }
         
         //while(!PORTBbits.RB4) {
